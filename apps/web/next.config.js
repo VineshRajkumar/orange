566f9117489306279d4f9d73baac7c551c9eafc3
VineshRajkumar <vineshrajkumar23@gmail.com>
2025-09-04 21:48:24 +0530

    adding nextjs-monorepo-workaround-plugin to fix deployment issue

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+
 const nextConfig = {
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Add PrismaPlugin to server-side webpack plugins
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  }
+
 };
 
 export default nextConfig;
