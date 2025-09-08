b158582579bc1f2de8c62b1d756491182be19106
VineshRajkumar <vineshrajkumar23@gmail.com>
2025-09-08 11:32:20 +0530

    changed guestlogin rate-limit to 5 per min

--- apps/http-backend/src/middlewares/guestLogin-rateLimiter.middleware.ts
+++ apps/http-backend/src/middlewares/guestLogin-rateLimiter.middleware.ts
@@ -1,9 +1,9 @@
 import rateLimit from "express-rate-limit";
 
-// limiting guest login to 10 requests per minute per IP
+// limiting guest login to 5 requests per minute per IP
 export const guestLoginLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 1,             // limit each IP to 10 requests per windowMs
+  max: 5,             // limit each IP to 5 requests per windowMs
   message: {
     success: false,
     message: "Too many guest login attempts. Please try again later."
