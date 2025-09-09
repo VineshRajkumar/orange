e3a7977fe8a7d2c05bdb5af62cae1f3a3890e934
VineshRajkumar <vineshrajkumar23@gmail.com>
2025-09-09 16:39:44 +0530

    rate limit for guest changed to 7request in 12hrs

--- apps/http-backend/src/middlewares/guestLogin-rateLimiter.middleware.ts
+++ apps/http-backend/src/middlewares/guestLogin-rateLimiter.middleware.ts
@@ -1,12 +1,12 @@
 import rateLimit from "express-rate-limit";
 
-// limiting guest login to 5 requests per minute per IP
+// limiting guest login to 7 requests in 12hrs per IP
 export const guestLoginLimiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 5,             // limit each IP to 5 requests per windowMs
+  windowMs: 12 * 60 * 60 * 1000, // 12 hours
+  max: 7,             // limit each IP to 7 requests per windowMs
   message: {
     success: false,
-    message: "Too many guest login attempts. Please try again later."
+    message: "Too many guest login attempts. You have reached the limit of 7 free guest sessions in the last 12 hours. Please log in with an account to continue."
   },
   standardHeaders: true, // send rate limit info in `RateLimit-*` headers -> rate limit info like what is the max limit and how many remaining requests etc 
   legacyHeaders: false,  // disable the `X-RateLimit-*` headers -> previous headers info regarding ratelimiting that might have saved dont consider that only new rate limiting info only
